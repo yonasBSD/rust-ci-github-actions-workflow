973625e334e5e9bb697c318012bd5e85e1b003b0
Yonas <yonas.y@gmail.com>
2024-10-16 07:09:20 -0400

    feat: Add shell completions and man pages generator.

--- backpack/src/lib.rs
+++ backpack/src/lib.rs
@@ -1,6 +1,23 @@
+use clap_allgen::{render_shell_completions, render_manpages};
+
+#[derive(Debug, clap::Parser)]
+enum Commands {
+    First,
+    Second,
+    Third,
+}
+
 /// Multiplies two integers
 pub fn multiply(a: i32, b: i32) -> i32 {
     a * b
+}
+
+pub fn generate_completions() {
+    render_shell_completions::<Commands>("~/.config/fish/completions").expect("shell completions generation failed");
+}
+
+pub fn generate_manpages() {
+    render_manpages::<Commands>("~/.local/share/man/man1").expect("man page generation failed");
 }
 
 #[cfg(test)]
@@ -8,7 +25,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_multiply() {
         assert_eq!(multiply(2, 2), 4);
+    }
+
+    #[test]
+    fn test_completions() {
+        assert_eq!("TODO", "TODO");
+    }
+
+    fn test_manpages() {
+        assert_eq!("TODO", "TODO");
     }
 }
--- backpack/src/main.rs
+++ backpack/src/main.rs
@@ -2,4 +2,7 @@ use test2::*;
 
 fn main() {
     println!("3 time 5 is {}", multiply(3, 5));
+
+    generate_completions();
+    generate_manpages();
 }
