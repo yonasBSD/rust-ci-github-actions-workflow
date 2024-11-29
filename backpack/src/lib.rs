a68c7beeec275cce7ec7ae7bf1036f86a0df389f
Yonas <yonas.y@gmail.com>
2024-11-29 15:41:03 -0500

    feat: Add BBD tests.

--- backpack/src/lib.rs
+++ backpack/src/lib.rs
@@ -6,9 +6,40 @@ pub fn multiply(a: i32, b: i32) -> i32 {
 #[cfg(test)]
 mod test {
     use super::*;
+    use demonstrate::demonstrate;
 
-    #[test]
-    fn test() {
-        assert_eq!(multiply(2, 2), 4);
+    demonstrate! {
+        describe "module" {
+            use super::*;
+
+            before {
+                let four = 4;
+            }
+
+            it "can fail" {
+                assert_eq!(multiply(2, 2), four);
+            }
+
+            test "is returnable" -> Result<(), &'static str> {
+                if multiply(2, 2) == four {
+                    Ok(())
+                } else {
+                    Err("It isn't 4! :o")
+                }
+            }
+
+            #[async_attributes::test]
+            async context "asynchronous" {
+                before {
+                    let is_4_task = async_std::task::spawn(async {
+                        multiply(2, 2)
+                    });
+                }
+
+                it "awaits" {
+                    assert_eq!(four, is_4_task.await)
+                }
+            }
+        }
     }
 }
