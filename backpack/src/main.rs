e7ce45ebec1f2df41dbb2c1b6470afa9a5e17965
Yonas Yanfa <yonas.y@gmail.com>
2025-12-22 18:35:28 -0500

    chore: Update upload-artifact actions/cache in README. (#97)
    [autofix.ci] apply automated fixes
    
    chore: Update actions/cache version in README

--- backpack/src/main.rs
+++ backpack/src/main.rs
@@ -5,10 +5,12 @@ use human_panic::{metadata, setup_panic};
 
 fn main() {
     #[cfg(not(target_arch = "wasm32"))]
-    setup_panic!(metadata!()
-        .authors("Acme Inc. <support@example.com")
-        .homepage("www.example.com")
-        .support("- Open a support request by email to support@example.com"));
+    setup_panic!(
+        metadata!()
+            .authors("Acme Inc. <support@example.com")
+            .homepage("www.example.com")
+            .support("- Open a support request by email to support@example.com")
+    );
 
     println!("3 time 5 is {}", multiply(3, 5));
 }
